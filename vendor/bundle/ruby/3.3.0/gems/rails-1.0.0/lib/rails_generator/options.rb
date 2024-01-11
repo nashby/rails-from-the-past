b69bb3759647b9f3ba69b561c76881363fa4e8d1
Vasiliy Ermolovich <younash@gmail.com>
2024-01-11 17:44:21 +0100

    options block var

--- vendor/bundle/ruby/3.3.0/gems/rails-1.0.0/lib/rails_generator/options.rb
+++ vendor/bundle/ruby/3.3.0/gems/rails-1.0.0/lib/rails_generator/options.rb
@@ -121,12 +121,12 @@ module Rails
           opt.separator ''
           opt.separator 'General Options:'
 
-          opt.on('-p', '--pretend', 'Run but do not make any changes.') { |o| o[:pretend] }
+          opt.on('-p', '--pretend', 'Run but do not make any changes.') { |o| options[:pretend] = o }
           opt.on('-f', '--force', 'Overwrite files that already exist.') { options[:collision] = :force }
           opt.on('-s', '--skip', 'Skip files that already exist.') { options[:collision] = :skip }
-          opt.on('-q', '--quiet', 'Suppress normal output.') { |o| o[:quiet] }
-          opt.on('-t', '--backtrace', 'Debugging: show backtrace on errors.') { |o| o[:backtrace] }
-          opt.on('-h', '--help', 'Show this help message.') { |o| o[:help] }
+          opt.on('-q', '--quiet', 'Suppress normal output.') { |o| options[:quiet] = o }
+          opt.on('-t', '--backtrace', 'Debugging: show backtrace on errors.') { |o| options[:backtrace] = o }
+          opt.on('-h', '--help', 'Show this help message.') { |o| options[:help] = o  }
           opt.on('-c', '--svn', 'Modify files with subversion. (Note: svn must be in path)') do
             options[:svn] = `svn status`.inject({}) do |opt, e|
               opt[e.chomp[7..-1]] = true
